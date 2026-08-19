c08f781da368c11f32a8653efa7226b0b7c049f7
Samis5 <samueluhrik8@gmail.com>
2026-08-19 17:23:34 +0200

    Integrate Google GenAI client for Q&A functionality

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from google import genai
+
+client = genai.Client(api_key= "AQ.Ab8RN6KBeVn-xZlltKEKXSALat1106sEnwWrNgY-KTk5iS4aSw")
+
+while True:
+    question = input("You:  ")
+
+    if question.lower() =="exit" : break
+
+    response = client.models.generate_content(
+            model ="gemini-3.6-flash",
+            contents=question
+    )
+
+    print("Gemini:",response.text)
 
